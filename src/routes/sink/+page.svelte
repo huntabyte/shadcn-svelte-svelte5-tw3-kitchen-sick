5d5187f467459028f2b3739e73ca831cc2893263
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-05-18 10:54:12 -0400

    add kitchen sink

---
+++ src/routes/sink/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import * as comps from "$lib/examples/index.js";
+</script>
+
+{#each Object.entries(comps) as [name, Comp]}
+	<div class="py-8 px-4 border">
+		<h1>{name}</h1>
+		<Comp />
+	</div>
+{/each}
