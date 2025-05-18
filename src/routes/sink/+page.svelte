f9fd3b8b6dedfcbbf12b195bbb703519c059eb43
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-05-18 11:43:06 -0400

    add more demos

--- src/routes/sink/+page.svelte
+++ src/routes/sink/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import * as comps from "$lib/examples/index.js";
 	import { Button } from "$lib/components/ui/button/index.js";
+	// needed for certain demos
+	import * as Tooltip from "$lib/components/ui/tooltip/index.js";
 
 	function changeMode() {
 		const el = document.documentElement;
@@ -11,9 +13,12 @@
 </script>
 
 <Button class="m-4" onclick={changeMode}>Change modes</Button>
-{#each Object.entries(comps) as [name, Comp]}
-	<div class="py-8 px-4 border">
-		<h1>{name}</h1>
-		<Comp />
-	</div>
-{/each}
+
+<Tooltip.Provider>
+	{#each Object.entries(comps) as [name, Comp]}
+		<div class="py-8 px-4 border">
+			<h1>{name}</h1>
+			<Comp />
+		</div>
+	{/each}
+</Tooltip.Provider>
