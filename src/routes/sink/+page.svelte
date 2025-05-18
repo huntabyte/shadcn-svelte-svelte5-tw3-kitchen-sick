77144add57a92760e682341626ef5898258726da
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-05-18 11:02:24 -0400

    toggle toggle

--- src/routes/sink/+page.svelte
+++ src/routes/sink/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import * as comps from "$lib/examples/index.js";
+	import { Button } from "$lib/components/ui/button/index.js";
+
+	function changeMode() {
+		const el = document.documentElement;
+		console.log(el);
+		if (el.className === "dark") el.className = "";
+		else el.className = "dark";
+	}
 </script>
 
+<Button class="m-4" onclick={changeMode}>Change modes</Button>
 {#each Object.entries(comps) as [name, Comp]}
 	<div class="py-8 px-4 border">
 		<h1>{name}</h1>
